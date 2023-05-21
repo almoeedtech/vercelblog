f96f26698f47f7d1670ca7ae22ca552dcd199426
almoeedtech <134221832+almoeedtech@users.noreply.github.com>
2023-05-21 21:57:25 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://designtutorials2009.blogspot.com/`
+					`https://designtutorials2009.blogspot.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
